e778908e26371c2a787aa732bef8ea3eb5e90204
mizliz5 <36713981+mizliz5@users.noreply.github.com>
2018-03-13 14:28:37 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,91 +1,100 @@
 // $('h1').html('Hello from js!');
 
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBohusPJGq_pHzmOqcGQjjIORI9HA_uxdI",
-    authDomain: "reservation-site-5c504.firebaseapp.com",
-    databaseURL: "https://reservation-site-5c504.firebaseio.com",
-    projectId: "reservation-site-5c504",
-    storageBucket: "reservation-site-5c504.appspot.com",
-    messagingSenderId: "688855184607"
-  };
+var config = {
+  apiKey: "AIzaSyBohusPJGq_pHzmOqcGQjjIORI9HA_uxdI",
+  authDomain: "reservation-site-5c504.firebaseapp.com",
+  databaseURL: "https://reservation-site-5c504.firebaseio.com",
+  projectId: "reservation-site-5c504",
+  storageBucket: "reservation-site-5c504.appspot.com",
+  messagingSenderId: "688855184607"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  
-
-// set the day when an option is clicked on
-$('.reservation-day li').on('click', function() {
-  reservationData.day = $(this).text();
-});
-
-// when submitted, the name data should be set
-$('.reservation-form').on('submit', function(event) {
-  event.preventDefault();
-
-  reservationData.name = $('.reservation-name').val();
-
-// create a section for reservations data in your db
-  var reservationsReference = database.ref('reservations');
-
-  reservationsReference.push(reservationData);
-});
-
-  // use reference to database to listen for changes in reservations data
-  database.ref('reservations').on('value', function(results) {
-
-      // Get all reservations stored in the results we received back from Firebase
-      var allReservations = results.val();
-
-      // remove all list reservations from DOM before appending list reservations
-      $('.reservations').empty();
-
-// iterate (loop) through all reservations coming from database call
-    for (var reservation in allReservations) {
-    // Create an object literal with the data we'll pass to Handlebars
-      var context = {
-        name: allReservations[reservation].name,
-        day: allReservations[reservation].day,
-        reservationId: reservation
-      };
-
-     // Get the HTML from our Handlebars reservation template
-      var source = $("#reservation-template").html();
-
-      // Compile our Handlebars template
-      var template = Handlebars.compile(source);
-
-      // Pass the data for this reservation (context) into the template
-      var reservationListItem = template(context);
-
-      // Append newly created reservation to reservations list.
-      $('.reservations').append(reservationListItem);
-
-    }
-
-  });
-
-}
-
-// When page loads, get reservations
-getReservations();
-
-
-
-  
+var database = firebase.database();
+var reservationData = {};
+    
 // google map
 function initMap() {
-  var monkLatLng = {lat: 40.8054491, lng: -73.9654415};
+  var monkLatLng = {lat: 40.996154, lng: 17.221659};
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: monkLatLng
   });
 
-// google map marker
+  // google map marker
   var marker = new google.maps.Marker({
     position: monkLatLng,
     map: map,
     title:"Monk's Cafe"
   });
 }
+
+// set the day when an option is clicked on
+$('.reservation-day').on('change', function() {
+  reservationData.day = $(this).val();
+  reservationData.name = $('.reservation-name').val()
+
+  console.log(reservationData);
+});
+
+// when submitted, the name data should be set
+$('.reservation-form').on('submit', function(event) {
+  event.preventDefault();
+
+  // create a section for reservations data in your db
+  var reservationsReference = database.ref('reservations');
+
+  reservationsReference.push(reservationData);
+  reservationData = {};
+});
+
+// use reference to database to listen for changes in reservations data
+database.ref('reservations').on('value', function(results) {
+
+  // Get all reservations stored in the results we received back from Firebase
+  var allReservations = results.val();
+
+  // remove all list reservations from DOM before appending list reservations
+  $('.reservation-list').empty();
+
+  // iterate (loop) through all reservations coming from database call
+  for (var reservation in allReservations) {
+    // Create an object literal with the data we'll pass to Handlebars
+    var context = {
+      name: allReservations[reservation].name,
+      day: allReservations[reservation].day,
+      reservationId: reservation
+    };
+
+   // Get the HTML from our Handlebars reservation template
+    var source = $("#reservation-template").html();
+
+    // Compile our Handlebars template
+    var template = Handlebars.compile(source);
+
+    // Pass the data for this reservation (context) into the template
+    var reservationListItem = template(context);
+
+    // Append newly created reservation to reservations list.
+    $('.reservation-list').append(reservationListItem);
+  }
+});
+
+// Smooth scrolling on navigation menu
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function() {
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+
+    return false;
+});
+
